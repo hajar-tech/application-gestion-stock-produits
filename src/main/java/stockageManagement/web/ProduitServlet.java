3790d04edea2a0add72903a3f0ba23bf6512c3ed
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-02-13 15:36:37 +0100

    fonction selectAll() et fonction delete()

--- src/main/java/stockageManagement/web/ProduitServlet.java
+++ src/main/java/stockageManagement/web/ProduitServlet.java
@@ -26,7 +26,8 @@ public class ProduitServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 
